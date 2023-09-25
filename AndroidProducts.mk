8700ca3cc1063e5784c241f919472a17ef3407b9
Andrea Puccio <andrea.puccio@metafyre.com>
2023-09-25 14:48:06 +0200

    AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,4 @@
+COMMON_LUNCH_CHOICES :=  \
+    lineage_goyawifi-user \
+    lineage_goyawifi-userdebug \
+    lineage_goyawifi-eng
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo lineage_goyawifi-user
-add_lunch_combo lineage_goyawifi-userdebug
-add_lunch_combo lineage_goyawifi-eng
